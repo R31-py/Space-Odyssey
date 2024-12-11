a84de913e82c617820e9256702c65ef80f0d9764
GersiP <gersipjetri18@gmail.com>
2024-12-11 23:11:05 +0100

    MainMenu_Final

--- Assets/Scripts/MainMenu/MainMenu.cs
+++ Assets/Scripts/MainMenu/MainMenu.cs
@@ -2,12 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-
+using UnityEngine.Audio;
+using UnityEngine.UI;
+//Tutorial: https://www.youtube.com/watch?v=ivvv8kld6_0
 public class MainMenu : MonoBehaviour
 {
-
+    public AudioMixer audioMixer;
+    
+    public Slider musicSlider;
+    public Slider sfxSlider;
+    
     public void StartGame()
     {
+        LoadVolume();
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 
@@ -15,5 +22,34 @@ public class MainMenu : MonoBehaviour
     {
         Debug.Log("Quit");
         Application.Quit();
+    }
+
+    //Grifft auf die Parameter von AudioMixer, die in UnityEditor gesetzt sind,zu und aktualisiert seinen Float-Wert
+    public void UpdateMusicVolume(float volume)
+    {
+        audioMixer.SetFloat("MusicVolume", volume);
+    }
+    
+    public void UpdateSFXVolume(float volume)
+    {
+        audioMixer.SetFloat("SfxVolume", volume);
+    }
+    
+    //speichert die Daten
+    public void SaveVolume()
+    {
+        audioMixer.GetFloat("MusicVolume", out float musicvolume);
+        PlayerPrefs.SetFloat("MusicVolume", musicvolume);
+        
+        audioMixer.GetFloat("SfxVolume", out float sfxvolume);
+        PlayerPrefs.SetFloat("SfxVolume", sfxvolume);
+    }
+    
+    //ladet die gespeicherte Daten in den jeweiligen Sliders
+    public void LoadVolume()
+    {
+        musicSlider.value = PlayerPrefs.GetFloat("MusicVolume");
+        sfxSlider.value = PlayerPrefs.GetFloat("SfxVolume");
+        
     }
 }
