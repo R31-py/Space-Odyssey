bfaed921c3dee9865b5939f1bc8803e20d673f74
GersiP <gersipjetri18@gmail.com>
2024-12-13 10:25:41 +0100

    PauseMenu + Minimap

--- Assets/Scripts/PauseMenu.cs
+++ Assets/Scripts/PauseMenu.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using DG.Tweening;
+using UnityEngine.SceneManagement;
 
 public class PauseMenu : MonoBehaviour
 {
@@ -44,6 +45,11 @@ public class PauseMenu : MonoBehaviour
         {
             Time.timeScale = 1;
         }
+    }
+
+    public void returnToMainMenu()
+    {
+        SceneManager.LoadScene("MainMenu");
     }
     
 }
